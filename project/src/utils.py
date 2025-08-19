de2ff3c3b60414a6e932755c65ccdc33ddc57ff3
Zhiang Qiu <zq2082@nyu.edu>
2025-08-18 20:03:42 -0400

    add

---
+++ project/src/utils.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def clean_dataframe(df, date_cols=None, rename=None):
+    if rename:
+        df = df.rename(columns=rename)
+
+    if date_cols:
+        for col in date_cols:
+            if col in df.columns:
+                df[col] = pd.to_datetime(df[col], errors="coerce")
+
+    return df
