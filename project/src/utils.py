46982821926db924e9f65992f27aa8b8bb066c38
Zhiang Qiu <zq2082@nyu.edu>
2025-08-27 08:51:39 -0400

    add

--- project/src/utils.py
+++ project/src/utils.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
 def clean_dataframe(df, date_cols=None, rename=None):
     if rename:
@@ -14,3 +16,22 @@ def clean_dataframe(df, date_cols=None, rename=None):
 
 def mae(y_true, y_pred):
     return float(np.mean(np.abs(y_true - y_pred)))
+
+
+def train_random_forest(df, feature_cols, target_col, n_estimators, test_size=0.25, random_state=7):
+    X = df[feature_cols]
+    y = df[target_col]
+
+    # Train/Test split
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=random_state
+    )
+
+    # Model
+    model = RandomForestClassifier(n_estimators=n_estimators, random_state=random_state)
+    model.fit(X_train, y_train)
+
+    # Predictions
+    y_pred = model.predict(X_test)
+
+    return model, X_test, y_test, y_pred
